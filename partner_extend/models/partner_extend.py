01771fdbdc5569e2357aaad753bbbaf6507920c7
abdelghani-khalidi <51909290+abdelghani-khalidi@users.noreply.github.com>
2023-10-23 16:11:07 +0100

    Update partner_extend.py

--- partner_extend/models/partner_extend.py
+++ partner_extend/models/partner_extend.py
@@ -64,7 +64,8 @@ class ExcelImportWizard(models.TransientModel):
                 'product_id': product.id,
                 'product_uom_qty': quantite,
                 'picking_id': reception.id,
-                'location_id': 1,
+                'location_id': 5,
+                'location_dest_id':8 , 
                 'lot_ids': lot_ids,
             })
 
